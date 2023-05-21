33466807b1609be18d166470a59d452fe1df451e
music <oscarelias414@gmail.com>
2023-05-20 20:55:13 -0600

    music-master

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+    content: ["./**/*.html"],
+    theme: {
+      extend: {
+        colors: {
+          'bg-body': '#FEFCF3',
+          'text-name':'#C58940',
+          'color-fav':'#CD9FD1',
+          
+          // 'class':'color',
+        },
+      },
+    },
+    plugins: [],
+  }
